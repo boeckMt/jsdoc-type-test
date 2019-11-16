b63604addb9bcf1a2342293d0852ba774d3f36d0
boeckMt <bbmatdd@gmail.com>
2019-11-16 22:12:59 +0100

    add jsconfig to type check js files with vs-code and tsconfig to type check js files with tsc

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// https://www.typescriptlang.org/docs/handbook/type-checking-javascript-files.html#supported-jsdoc
+
+/**
+ * @type {string | number}
+ */
+let message = 'hello world';
+message = 1;
